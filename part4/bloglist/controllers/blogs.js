da4a3de7b133c1a43e5ab7d0c30b53d811db9380
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-10-19 22:22:08 +0300

    Add solution to exercise 4.22

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,5 @@
 const blogsRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
-const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -9,14 +7,8 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({
-      error: 'Invalid token.'
-    })
-  }
-  const user = await User.findById(decodedToken.id)
   const blog = new Blog(request.body)
+  const user = request.user
   blog.user = user.id
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog.id)
@@ -25,14 +17,8 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({
-      error: 'Invalid token.'
-    })
-  }
   const blog = await Blog.findById(request.params.id)
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
   if (blog.user.toString() === user._id.toString()) {
     await Blog.findByIdAndDelete(request.params.id)
     user.blogs = user.blogs.filter((blog) => blog.toString() !== request.params.id)
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,10 +1,5 @@
-const tokenExtractor = (request, response, next) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    request.token = authorization.replace('bearer ', '')
-  }
-  next()
-}
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
@@ -19,7 +14,28 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.replace('bearer ', '')
+  } else {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    request.user = await User.findById(decodedToken.id)
+  } else {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  next()
+}
+
 module.exports = {
+  errorHandler,
   tokenExtractor,
-  errorHandler
+  userExtractor
 }
