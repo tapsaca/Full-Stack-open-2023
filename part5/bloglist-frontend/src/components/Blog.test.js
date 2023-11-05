f072edb0dafcd831c8154d6cd50b6fdf38151102
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-11-05 18:20:04 +0200

    Add solution to exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,8 +1,9 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author but no URL or likes', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'title',
     author: 'author',
@@ -13,15 +14,32 @@ test('renders title and author but no URL or likes', () => {
       username: 'username'
     }
   }
+
   const user = {
     name: 'name',
     username: 'username'
   }
-  render(<Blog blog={blog} user={user} />)
-  const text = screen.queryByText('title, author')
-  expect(text).not.toBeNull()
-  const url = screen.queryByText('url')
-  expect(url).not.toBeVisible()
-  const likes = screen.queryByText('0')
-  expect(likes).not.toBeVisible()
+
+  beforeEach(() => {
+    render(<Blog blog={blog} user={user} />)
+  })
+
+  test('renders title and author but no URL or likes', () => {
+    const text = screen.queryByText('title, author')
+    expect(text).not.toBeNull()
+    const url = screen.queryByText('url')
+    expect(url).not.toBeVisible()
+    const likes = screen.queryByText('0')
+    expect(likes).not.toBeVisible()
+  })
+
+  test('renders URL and likes when button is pressed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+    const url = screen.queryByText('url')
+    expect(url).toBeVisible()
+    const likes = screen.queryByText('0')
+    expect(likes).toBeVisible()
+  })
 })
