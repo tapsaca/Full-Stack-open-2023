8d176910346d56baf84434c70a67a421ea590f23
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-11-08 23:46:40 +0200

    Add solution to exercise 5.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -16,6 +16,10 @@ app.use(express.json())
 app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor, blogsRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 app.use(middleware.errorHandler)
 
 module.exports = app
