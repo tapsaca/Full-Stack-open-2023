abbf01dbcf76fb1d1bdf3860dbdfd70a4d6672e6
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-11-16 16:07:11 +0200

    Add solution to exercise 6.2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { createStore } from 'redux'
 import reducer from './reducer'
 
@@ -15,13 +14,13 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button> 
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button> 
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
