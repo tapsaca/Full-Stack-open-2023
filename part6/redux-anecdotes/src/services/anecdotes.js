81836c10179a841afe0993b4fed975d8d4d44b3e
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-11-28 05:39:15 +0200

    Add solution to exercise 6.15

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -2,9 +2,14 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+const create = async (content) => {
+  const response = await axios.post(baseUrl, { content, votes: 0 })
+  return response.data
+}
+
 const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
 
-export default { getAll }
+export default { create, getAll }
