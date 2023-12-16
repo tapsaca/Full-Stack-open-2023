b4e811e617a9e7ac9012954fc4c8a556b627f79d
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-12-16 13:27:01 +0200

    Add solution to exercise 7.8

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const getAll = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    getAll()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
