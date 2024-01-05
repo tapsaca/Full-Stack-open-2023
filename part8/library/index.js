601b4f715e2843cd285c8ef1fd2632a818d24adc
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-01-05 23:13:04 +0200

    Add solution to exercise 8.2

--- part8/library/index.js
+++ part8/library/index.js
@@ -94,7 +94,16 @@ let books = [
 ]
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
+    allBooks: [Book!]!
     authorCount: Int!
     bookCount: Int!
   }
@@ -102,6 +111,7 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
+    allBooks: () => books,
     authorCount: () => authors.length,
     bookCount: () => books.length
   }
