7587a8e766248b00436c81bc14c4933150fcb347
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-01-21 17:00:46 +0200

    Add solution to exercise 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,15 +1,19 @@
 import { useQuery } from '@apollo/client'
+import { useState } from 'react'
 import { ALL_BOOKS } from '../queries'
 
 const Books = () => {
   const books = useQuery(ALL_BOOKS)
+  const [filter, setFilter] = useState('')
 
   if (books.loading) return null
+
+  const genres = [...new Set(books.data.allBooks.map((b) => b.genres).flat())]
 
   return (
     <div>
       <h2>books</h2>
-
+      {!filter ? null : <div>in genre {filter}</div>}
       <table>
         <tbody>
           <tr>
@@ -17,15 +21,25 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.data.allBooks
+            .filter((b) => (!filter ? true : b.genres.includes(filter)))
+            .map((b) => (
+              <tr key={b.title}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((g) => (
+          <button key={g} onClick={() => setFilter(g)}>
+            {g}
+          </button>
+        ))}
+        <button onClick={() => setFilter('')}>all genres</button>
+      </div>
     </div>
   )
 }
